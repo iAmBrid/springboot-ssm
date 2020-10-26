fd2f48d32f542079c9dcbd2b59924ec1045fee83
jianpeng_zhang <jianpeng.zhang@gameday.tech>
2020-10-26 16:56:30 +0800

    初始化项目

---
+++ src/main/java/com/duoyi/springbootssm/SpringbootSsmApplication.java
@@ -0,0 +1,13 @@
+package com.duoyi.springbootssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSsmApplication.class, args);
+    }
+
+}
