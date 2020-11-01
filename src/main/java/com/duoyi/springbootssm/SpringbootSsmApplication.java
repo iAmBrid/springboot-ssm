f077a5db5271d856fb99d80c4334d87b35b19ad3
jianpeng_zhang <jianpeng.zhang@gameday.tech>
2020-11-01 10:31:51 +0800

    添加c3p0数据连接池，完成ssm配置

--- src/main/java/com/duoyi/springbootssm/SpringbootSsmApplication.java
+++ src/main/java/com/duoyi/springbootssm/SpringbootSsmApplication.java
@@ -1,13 +1,10 @@
 package com.duoyi.springbootssm;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan({"com.duoyi.springbootssm.mapper"})
 @SpringBootApplication
 public class SpringbootSsmApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSsmApplication.class, args);
     }
---
+++ src/main/java/com/duoyi/springbootssm/config/SSMConfiguration.java
@@ -0,0 +1,45 @@
+package com.duoyi.springbootssm.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+
+@Configuration
+@MapperScan("com.duoyi.springbootssm.mapper")
+public class SSMConfiguration {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean(name = "dataSource")
+    public ComboPooledDataSource createDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://47.97.68.197:3307/springbootssm?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B8&allowPublicKeyRetrieval=true");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        dataSource.setAutoCommitOnClose(false);
+        return dataSource;
+    }
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactory() throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        String packageSearchPath = "classpath*:/mapper/**.xml";
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.duoyi.springbootssm.domain");
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
+}
